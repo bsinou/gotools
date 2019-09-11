05d629f258bf89bbd3b3bf66e1e0e77f7f61c007
Bruno Sinou <bruno.sinou@posteo.de>
2019-09-11 10:40:55 +0200

    Add comparison command

--- cmd/versions.go
+++ cmd/versions.go
@@ -36,7 +36,40 @@ var ivCmd = &cobra.Command{
 	},
 }
 
+var igtCmd = &cobra.Command{
+	Use: "isgreater",
+	Long: `Tries to parse passed versions as string using the hashicorp/go-version library and returns an error if:
+	- one of the 2 strings cannot be parsed
+	- the first version is not strictly greater than the second`,
+	Run: func(cm *cobra.Command, args []string) {
+		if len(args) != 2 {
+			cm.Printf("Please provide two versions to be compared\n")
+			os.Exit(1)
+		}
+
+		v1Str := args[0]
+		v2Str := args[1]
+		fmt.Printf("Comparing versions %s & %s \n", v1Str, v2Str)
+
+		v1, err := version.NewVersion(v1Str)
+		if err != nil {
+			cm.Printf("Passed version [%s] is not a valid version\n", v1Str)
+			os.Exit(1)
+		}
+		v2, err := version.NewVersion(v2Str)
+		if err != nil {
+			cm.Printf("Passed version [%s] is not a valid version\n", v2Str)
+			os.Exit(1)
+		}
+		if !v1.GreaterThan(v2) {
+			cm.Printf("Passed version [%s] is *not* greater than [%s]\n", v1Str, v2Str)
+			os.Exit(1)
+		}
+	},
+}
+
 func init() {
 	verCmd.AddCommand(ivCmd)
+	verCmd.AddCommand(igtCmd)
 	RootCmd.AddCommand(verCmd)
 }
